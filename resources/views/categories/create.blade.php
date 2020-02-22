29103843f0e13915fc99bed005f56996b48c9b5f
Maidul Islam <34934101+vatia60@users.noreply.github.com>
2020-02-22 11:02:01 +0600

    add code

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+@section('content')
+<div class="category">
+  <div class="container">
+    <div class="col-md-6 offset-md-3">
+    <h2>Category</h2>
+      <form action="">
+        <div class="form-group">
+          <input name="" value="" type="text" class="form-control" placeholder="">
+        </div>
+        <div class="form-group">
+          <input name="" value="" type="text" class="form-control" placeholder="">
+        </div>
+        <button type="submit" class="btn btn-success">Create Category</button>
+      </form>
+    </div>
+  </div>
+</div>
+@endsection
